4fec96f862d95d82b3260d3c40fe377a81b1b0b6
Kalos77 <kevincheutin@gmail.com>
2013-04-24 16:28:49 +0200

    Premiere Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cvcollect::Application.config.secret_token = '3d373f3461361f3f13a19dcea45ace9165c187d5bfb0e8bbee36411c2e16a1cb0435b8881ecb1df4bbdb9372e9e26590832e37d01d5d68dd991b5d3a6e509e78'
